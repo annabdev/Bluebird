5d84a40b24a77c1c9c9a0faaa1746054d73515aa
Anna Beagley <beagley.dev@gmail.com>
2019-06-03 11:14:30 -0600

    initial commit

---
+++ src/components/Auth/Login.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Login extends Component {
+    render() {
+        return (
+            <div>
+
+                This is the Login page
+            </div>
+        )
+    }
+}
+
+export default Login;
